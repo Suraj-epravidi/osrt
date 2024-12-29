5cf31900db5042698b5aa59549085c45b1d49d91
Suraj-epravidi <suraj@epravidi.com>
2024-12-29 13:46:23 +0545

    Changes

--- brands.php
+++ brands.php
@@ -252,10 +252,10 @@ $conn->close();
                 </div>
             </div>
             <?php
-        $servername = "192.250.235.20";
-        $username = "epravidi_osrt_data";
-        $password = "UQ!r.gTOz=oo";
-        $dbname = "epravidi_osrt";
+      $servername = "localhost";
+      $username = "osrtraders_epravidi";
+      $password = "UQ!r.gTOz=oo";
+      $dbname = "osrtraders_epravidi_osrt";
 
         $conn = new mysqli($servername, $username, $password, $dbname);
 
